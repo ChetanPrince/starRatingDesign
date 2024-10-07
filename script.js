8e556b3f50678ab48de8abdfc6220137a7e53cf4
ChetanPrince <saini.rajvir2@gmail.com>
2024-10-07 20:49:10 +0200

    javascript added with getting elements by id and classNames

---
+++ script.js
@@ -0,0 +1,12 @@
+let stars = document.getElementsByClassName("star");
+let output = document.getElementById("output");
+
+
+function gfg(n){
+    for(let i=0; i<n; i++){
+        if(n==1){
+
+        }
+    }
+
+}
